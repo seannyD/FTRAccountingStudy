2d1825dcf25093644c214e3338fe5c70a30c5c67
seannyD <sean.g.roberts@gmail.com>
2018-08-24 11:55:38 +1000

    update cluster loading

--- analysis/multiMembershipModel_cluster.R
+++ analysis/multiMembershipModel_cluster.R
@@ -1,21 +1,18 @@
 library(brms)
 library(parallel)
 
-
 numChains = 8
 
-
-d = read.csv("../data/clean/data.csv",
-             fileEncoding = "utf-8",
-             encoding = 'utf-8')
+load("../data/clean/data_min.Rdat")
 
 fit_mm <- 
   brm(AAM ~ 1 + pcftr + 
         (1 | mm(G1, G2, G3, # genuses
                 weights = cbind(G1.p, G2.p, G3.p))), 
       data   = d,
-      warmup = 50,
-      iter   = 100,
-      chains = numChains)
+      warmup =  1000,
+      iter   = 11000,
+      chains = numChains,
+      cores = numChains)
 
 save(fit_mm, file="../results/modelResults/AAM_vs_pcftr_MMME.Rdat")
